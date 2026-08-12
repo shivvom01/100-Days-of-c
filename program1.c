7bd70ee4d23bb257c4108daa5b54c112bd59d79e
shivvom01 <shivomr05@gmail.com>
2026-08-12 10:57:16 +0530

    Create program1.c

---
+++ program1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,sum,div,multi,sub;
+printf("Enter first number");
+scanf("%d",&a);
+printf("\nEnter second number");
+scanf("%d",&b);
+sum=a+b;
+printf("\n The sum of the two numbers is=%d",sum);
+getch();
+}
