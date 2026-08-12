c30ee581a824f649a711d4e8baaef5b96c44994a
shivvom01 <shivomr05@gmail.com>
2026-08-12 11:32:01 +0530

    Create day2ques2.c

---
+++ day2ques2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int r,circum,area;
+printf("Enter the radius of a circle");
+scanf("%d",&r);
+circum=2*3.14*r;
+area=3.14*r*r;
+printf("\nCircumference of the circle is=%d",circum);
+printf("\nArea of the circle is=%d",area);
+getch();
+}
