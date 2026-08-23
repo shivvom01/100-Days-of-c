dae4557290e583de2f0977df2b92aaad987b4508
shivvom01 <shivomr05@gmail.com>
2026-08-23 21:56:05 +0530

    Create day14ques2.c

---
+++ day14ques2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, product = 1;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i = i + 2)
+    {
+        product = product * i;
+    }
+
+    printf("Product of even numbers from 1 to %d = %d", n, product);
+
+    return 0;
+}
